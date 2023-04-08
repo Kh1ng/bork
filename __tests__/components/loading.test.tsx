8378389e4572574dd21f1123f200bf07e1c0c4c6
kh1ng <khing@pm.me>
2023-04-08 04:51:13 -0500

    layout to components, more testing

---
+++ __tests__/components/loading.test.tsx
@@ -0,0 +1,26 @@
+import mockRouter from "next-router-mock";
+import useUser from "@clerk/nextjs";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import {
+  LoadingDog,
+  LoadingPage,
+  LoadingSpinner,
+} from "../../src/components/loading";
+
+describe("Loading component", () => {
+  it("renders the LoadingDog component", () => {
+    const wrapper = render(<LoadingDog />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it("renders the LoadingPage component", () => {
+    const wrapper = render(<LoadingPage />);
+    expect(wrapper).toBeDefined();
+  });
+
+  it("renders the LoadingSpinner component", () => {
+    const wrapper = render(<LoadingSpinner />);
+    expect(wrapper).toBeDefined();
+  });
+});
