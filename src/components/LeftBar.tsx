ec1f88e6b4e89e9d29b9c3ddac5173f570949c6c
Colton Spurgin <khing@pm.me>
2024-04-27 09:49:10 -0500

    accout section bottom of left menu formatting, probably broke responsiveness.

--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -8,7 +8,7 @@ const LeftBar = () => {
   const { user } = useUser();
   if (!user?.username)
     return (
-      <div className="flex h-full w-full flex-col pr-4 pt-4">
+      <div className="flex h-full min-w-36 flex-col pr-4 pt-4">
         <nav className="m-auto gap-2">
           <Link href="/">Home</Link>
           <Link className="flex" href="https://github.com/kh1ng/bork">
@@ -27,21 +27,28 @@ const LeftBar = () => {
             About
           </Link>
         </nav>
-        <div className="flex h-full w-full flex-col justify-end gap-2 pb-4 pl-2 align-text-bottom">
-          <div className="flex flex-row justify-center ">
-            <Image
-              src={user?.imageUrl.toString()}
-              className="h-14 w-14 rounded-full"
-              alt={`@${user?.username.toString()}'s profile picture`}
-              width={56}
-              height={56}
-            />
-            <div className="justify-end pl-4 pr-2">
-              <Link href={`/@${user.username}`}>
-                <span>{`@${user.username} `}</span>
-              </Link>
-              <br />
-              <SignOutButton />
+        <div className="flex h-full min-w-24 flex-col justify-end gap-2 p-4 align-text-bottom">
+          <div className="flex flex-row ">
+            <div className="grid grid-cols-2 grid-rows-1 gap-1">
+              <div>
+                <Image
+                  src={user?.imageUrl.toString()}
+                  className="flex h-14 w-14 place-self-center self-center rounded-full"
+                  alt={`@${user?.username.toString()}'s profile picture`}
+                  width={56}
+                  height={56}
+                />
+
+                <Link className="" href={`/@${user.username}`}>
+                  {`@${user.username} `}
+                </Link>
+              </div>
+              <div className="self-end">
+                <Link className="" href="UserSettingsPage">
+                  Account
+                </Link>
+                <SignOutButton />
+              </div>
             </div>
           </div>
         </div>
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,12 +5,13 @@ export const PageLayout = (props: PropsWithChildren) => {
   return (
     <>
       <main className="flex h-screen justify-center overflow-hidden">
-        <div className="flex h-full text-slate-100">
+        <div className="flex h-full w-1/4 place-self-start text-slate-200">
           <LeftBar />
         </div>
         <div className="flex h-full w-full flex-col border-x border-slate-400 md:max-w-2xl">
           {props.children}
         </div>
+        <div className="flex h-full w-1/4 place-self-end text-slate-100"></div>
       </main>
     </>
   );
